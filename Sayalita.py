b6c9d5d30d322a3c07b55f1d62895b7168b13c7d
Carter Garcia <74512748+Sayarinu@users.noreply.github.com>
2023-06-14 13:56:50 -0400

    Add files via upload

---
+++ Sayalita.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.typing = False
+intents.presences = False
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name} ({bot.user.id})')
+
+with open('token.txt', 'r') as file:
+    token = file.read()
+
+bot.run(token)
